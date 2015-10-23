c9d9aff0fd6ce333938bf19a7e8bc540f5aaa028
JamesFoxChen <25717594@qq.com>
2015-10-23 17:12:02 +0800

    WeiXin

---
+++ WeiXinCustomMenu/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeiXinCustomMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
